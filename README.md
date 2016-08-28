1a0ccc26d8cb086afc07324726b3a5fc1d3685ad
Eduardo Shanahan <contact@eduardoshanahan.com>
2016-08-28 08:19:56 +0100

    Updated instructions

--- README.md
+++ README.md
@@ -8,9 +8,17 @@ I am using it against OVH, but it should work in AWS EC2 too.
 
 If you run in your terminal
 ```bash
-docker-compose run deployment
+docker-compose run --rm deployment
 ```
 you will be left in a session where you can get a list of the fabric commands with a
 ```bash
 fab -l
+```
+
+## Development
+
+If you want to make some changes and version it, [bumpversion](https://pypi.python.org/pypi/bumpversion) is configured
+
+```bash
+bumpversion patch
 ```
--- scripts/fabfile.py
+++ scripts/fabfile.py
@@ -19,7 +19,7 @@ def prepare_new_server():
     Get a server and make it ready to be used in production
     '''
     username = os.environ['REMOTE_USERNAME']
-    key_path = os.environ['REMOTE_PUBLIC_KEY_PATH'] 
+    key_path = os.environ['REMOTE_PUBLIC_KEY_PATH']
     setup_key_access(username, key_path)
     setup_docker()
 
@@ -79,6 +79,9 @@ def restart_ssh():
 
 @task
 def setup_docker():
+    """
+    Install docker and setup a 'deploy' user
+    """
     install_docker()
     docker_user = 'deploy'
     create_user(docker_user)
